b5078686c4984021184285baae8c41a6b02cecd4
harish prajapat <prajapath8269@gmail.com>
2026-08-14 13:26:52 +0530

    change C to c

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
-import { MovieProvider } from "./context/MovieContext";
+import { MovieProvider } from "./context/Moviecontext";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
